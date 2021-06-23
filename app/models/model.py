833685b68a81ae1faab7f556849c4eabb2d5a8ba
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-06-23 00:26:39 -0500

    Se creo la clase base de los modelos

---
+++ app/models/model.py
@@ -0,0 +1,23 @@
+
+class Model(dict):
+    __getattr__ = dict.get
+    __delattr__ = dict.__delattr__
+    __setattr__ = dict.__setattr__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert_one(self)
+        else:
+            self.collection.replace_one({"_id": self._id}, self)
+
+    def reload(self):
+        if self._id:
+            self.update(self.collection.find_one({"_id": self._id}))
+
+    def remove(self):
+        if self._id:
+            self.collection.delete_one({"_id": self._id})
+            self.clear()
+
+    def find(self, filters: dict):
+        self.update(self.collection.find_one(filters))
--- app/services/authentication.py
+++ app/services/authentication.py
@@ -8,7 +8,8 @@ def protected_route(func):
     def __validate_jwt(*args, **kwargs):
         token = request.headers.get('Authorization').split(" ")[1]
         try:
-            token_data = jwt.decode(token, app.config.get("SECRET_KEY"), algorithms=["HS256"])
+            token_data = jwt.decode(
+                token, app.config.get("SECRET_KEY"), algorithms=["HS256"])
             if type(token_data) == dict:
                 func()
             else:
