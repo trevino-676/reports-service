367d30fdfb4d9e3c17886dca434cf1cf8832ec05
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-06-24 13:19:39 -0500

    Se agregaron las rutas de los reportes de ventas a la aplicacion de flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 
+from app.routes import sells_by_client_routes
+
 app = Flask(__name__)
 app.config.from_object("config.Config")
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
 mongo = PyMongo(app, authSource="admin")
+
+
+app.register_blueprint(sells_by_client_routes)
