139c895fa1f2b8a58100a6aad8d13ba731d361ec
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-02 14:32:46 -0500

    Se creo el repositorio del reporte de percepciones gravables

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,6 +1,8 @@
 from app.repository.repository import ReportRepository
 from app.repository.sells_by_client import Sells_By_Client_Repository
 from app.repository.sell_by_item import SellsByItemsRepository
+from app.repository.taxables_perceptions import TaxablesPerceptionsRepository
 
 sells_clients_repo = Sells_By_Client_Repository()
 sells_by_items_repo = SellsByItemsRepository()
+taxables_perceptions_repo = TaxablesPerceptionsRepository()
---
+++ app/repository/taxables_perceptions.py
@@ -0,0 +1,21 @@
+import uuid
+
+from app import app
+from app.models import TaxablesPerceptions
+from app.repository import ReportRepository
+
+
+class TaxablesPerceptionsRepository(ReportRepository):
+    def get_report(self, filters: dict) -> list:
+        try:
+            report = TaxablesPerceptions.get_taxables_perceptions(filters)
+            if len(report) == 0:
+                return None
+
+            for item in report:
+                item["uuid"] == uuid.uuid4().hex
+
+            return report
+        except Exception as e:
+            app.logger.error(e)
+            return None
