220f7d67d6d566901799f9052fdd2305bc2178d4
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-02 14:22:00 -0500

    Se creo el modelo para el reporte de percepciones gravables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from app.models.model import Model
 from app.models.sells_client import Sells_By_Client
 from app.models.sells_by_items import SellsByItems
+from app.models.taxables_perceptions import TaxablesPerceptions
---
+++ app/models/taxables_perceptions.py
@@ -0,0 +1,45 @@
+from app.models import Model
+from app import app, mongo
+
+
+class TaxablesPerceptions(Model):
+    """
+    Modelo para el reporte de percepciones gravables.
+    """
+
+    collection_name = app.config["NOMINAS_COLLECTION"]
+    collection = mongo.db[collection_name]
+
+    @classmethod
+    def get_taxables_perceptions(cls, filters: dict) -> list:
+        pipeline = [
+            {"$match": filters},
+            {"$unwind": "$nomina.Percepciones.Percepcion"},
+            {
+                "$group": {
+                    "_id": {"empleado": "$Receptor.Nombre", "rfc": "$Receptor.Rfc"},
+                    "total_grabado": {
+                        "$sum": {"$toDouble": "$nomina.Percepciones.TotalGravado"}
+                    },
+                    "total_sueldo": {
+                        "$sum": {"$toDouble": "$nomina.Percepciones.TotalSueldos"}
+                    },
+                    "total_exento": {
+                        "$sum": {"$toDouble": "$nomina.Percepciones.TotalExento"}
+                    },
+                    "tipo_percepcion": {
+                        "$push": "$nomina.Percepciones.Percepcion.TipoPercepcion"
+                    },
+                    "claves": {"$push": "$nomina.Percepciones.Percepcion.Clave"},
+                    "conceptos": {"$push": "$nomina.Percepciones.Percepcion.Concepto"},
+                    "importes_gravados": {
+                        "$push": "$nomina.Percepciones.Percepcion.ImporteGravado"
+                    },
+                    "importes_exento": {
+                        "$push": "$nomina.Percepciones.Percepcion.ImporteExento"
+                    },
+                }
+            },
+        ]
+        data = cls.collection.aggregate(pipeline=pipeline)
+        return list(data)
