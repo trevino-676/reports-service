ddf123c80b322a09708a27895dba9c9375e4b6bb
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-06 13:27:05 -0500

    Se creo el modelo de SellsByServices para el reporte de ventas por servicios

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.model import Model
 from app.models.sells_client import Sells_By_Client
 from app.models.sells_by_items import SellsByItems
 from app.models.taxables_perceptions import TaxablesPerceptions
+from app.models.sells_by_services import SellsByServices
---
+++ app/models/sells_by_services.py
@@ -0,0 +1,35 @@
+from app.models import Model
+from app import app, mongo
+
+
+class SellsByServices(Model):
+    collection_name = app.config["PRINCIPAL_COLLECTION"]
+    collection = mongo.db[collection_name]
+
+    @classmethod
+    def get_sells_by_services(cls, filters: dict) -> list:
+        pipeline = [
+            {"$match": filters},
+            {"$unwind": "$conceptos"},
+            {
+                "$match": {
+                    "conceptos.Detalles.ClaveUnidad": {"$in": ["E48", "ACT", "AS", "E54"]}
+                }
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "servicio": "$conceptos.Detalles.Descripcion",
+                        "codigo": "$conceptos.Detalles.ClaveProdServ",
+                        "clave": "$conceptos.Detalles.ClaveUnidad",
+                    },
+                    "cantidad": {"$sum": {"$toDouble": "$conceptos.Detalles.Cantidad"}},
+                    "precio_unitario": {
+                        "$first": {"$toDouble": "$conceptos.Detalles.ValorUnitario"}
+                    },
+                    "importe": {"$sum": {"$toDouble": "$conceptos.Detalles.Importe"}},
+                }
+            },
+        ]
+        sells = cls.collection.aggregate(pipeline=pipeline)
+        return list(sells)
