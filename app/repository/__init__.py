5fba405dbcc0d5b88a34e4402f73a999b2cdcc8d
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-13 00:01:05 -0500

    Se creo el repsitorio de datos para el reporte de retenciones

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -3,8 +3,10 @@ from app.repository.sells_by_client import Sells_By_Client_Repository
 from app.repository.sell_by_item import SellsByItemsRepository
 from app.repository.taxables_perceptions import TaxablesPerceptionsRepository
 from app.repository.sells_by_services import SellsByServicesRepository
+from app.repository.retention_report import RetentionRepository
 
 sells_clients_repo = Sells_By_Client_Repository()
 sells_by_items_repo = SellsByItemsRepository()
 taxables_perceptions_repo = TaxablesPerceptionsRepository()
 sells_by_services_repo = SellsByServicesRepository()
+retention_repo = RetentionRepository()
---
+++ app/repository/retention_report.py
@@ -0,0 +1,33 @@
+import uuid
+
+from app.models import RetentionReportModel
+from app.repository import Repository
+from app import app
+
+
+class RetentionRepository(Repository):
+    """
+    Esta clase contiene el metodo para obtener el reporte.
+    """
+    def get_report(self, filters: dict) -> list:
+        """
+        Manda a llamar el metodo para generar la informacion del
+        reporte de retenciones
+
+        Params:
+            filters (dict): Filtros para generar el reporte.
+
+        Returns:
+            Una lista con los datos del reporte de retenciones.
+        """
+        try:
+            report = RetentionReportModel.get_retention_report(filters)
+            if len(report) == 0:
+                raise Exception("No se encontraron datos para el reporte")
+            for item in report:
+                item["uuid"] = uuid.uuid4().hex
+            return report
+        except Exception as e:
+            app.logger.error(e)
+            return None
+
