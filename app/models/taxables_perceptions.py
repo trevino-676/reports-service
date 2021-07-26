baddec0aa897ca455a024abf761f83032e4a965c
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-26 13:14:11 -0500

    Se agrego el campo de fecha_pago a la informacion del reporte de percepciones grabavles

--- app/models/taxables_perceptions.py
+++ app/models/taxables_perceptions.py
@@ -16,19 +16,28 @@ class TaxablesPerceptions(Model):
             {"$match": filters},
             {"$addFields": {"percepciones": "$nomina.Percepciones.Percepcion"}},
             {"$unwind": "$percepciones"},
-            {"$group": {
-                "_id": {"empleado": "$Receptor.Nombre", "rfc": "$Receptor.Rfc"},
-                "total_grabado": {"$sum": {"$toDouble": "$percepciones.ImporteGravado"}},
-                "total_exento": {"$sum": {"$toDouble": "$percepciones.ImporteExento"}},
-                "tipo_percepcion": {"$push": "$percepciones.TipoPercepcion"},
-                "claves": {"$push": "$percepciones.Clave"},
-                "conceptos": {"$push": "$percepciones.Concepto"},
-                "importes_gravados": {"$push": "$percepciones.ImporteGravado"},
-                "importes_exento": {"$push": "$percepciones.ImporteExento"}
-            }},
-            {"$addFields": {
-                "total_sueldo": {"$sum": ["$total_grabado", "$total_exento"]}
-            }},
+            {
+                "$group": {
+                    "_id": {"empleado": "$Receptor.Nombre", "rfc": "$Receptor.Rfc"},
+                    "total_grabado": {
+                        "$sum": {"$toDouble": "$percepciones.ImporteGravado"}
+                    },
+                    "total_exento": {
+                        "$sum": {"$toDouble": "$percepciones.ImporteExento"}
+                    },
+                    "tipo_percepcion": {"$push": "$percepciones.TipoPercepcion"},
+                    "claves": {"$push": "$percepciones.Clave"},
+                    "conceptos": {"$push": "$percepciones.Concepto"},
+                    "importes_gravados": {"$push": "$percepciones.ImporteGravado"},
+                    "importes_exento": {"$push": "$percepciones.ImporteExento"},
+                    "fecha_pago": {"$push": "$nomina.FechaPago"},
+                }
+            },
+            {
+                "$addFields": {
+                    "total_sueldo": {"$sum": ["$total_grabado", "$total_exento"]}
+                }
+            },
             {"$sort": {"_id.empleado": 1}},
         ]
         data = cls.collection.aggregate(pipeline=pipeline)
